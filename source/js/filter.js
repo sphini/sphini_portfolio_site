7c35ec3ae557a022ee91d30efbd58d27fb601eeb
Sphini <spdop2000@gmail.com>
2020-01-10 19:56:53 +0500

    Реализовал работу фильтра. Подписал комментарии в коде.

--- source/js/filter.js
+++ source/js/filter.js
@@ -1,71 +1,36 @@
-// Execute the function and clear all columns
-// function filterSelection(c) {
-  // var x, i;
-  // x = document.getElementsByClassName("portfolio-content__image");
-  // if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  // for (i = 0; i < x.length; i++) {
-    // w3RemoveClass(x[i], "show");
-//     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-//   }
-// }
-
-
-// Show filtered elements
-// function w3AddClass(element, name) {
-//   var i, arr1, arr2;
-//   arr1 = element.className.split(" ");
-//   arr2 = name.split(" ");
-//   for (i = 0; i < arr2.length; i++) {
-//     if (arr1.indexOf(arr2[i]) == -1) {
-//       element.className += " " + arr2[i];
-//     }
-//   }
-// }
-
-// Hide elements that are not selected
-// function w3RemoveClass(element, name) {
-//   var i, arr1, arr2;
-//   arr1 = element.className.split(" ");
-//   arr2 = name.split(" ");
-//   for (i = 0; i < arr2.length; i++) {
-//     while (arr1.indexOf(arr2[i]) > -1) {
-//       arr1.splice(arr1.indexOf(arr2[i]), 1);
-//     }
-//   }
-//   element.className = arr1.join(" ");
-// }
-
 var itemCollection = document.getElementsByClassName("portfolio-content__image");
-for (var i = 0; i < itemCollection.length; i++) {
-  currentItem = itemCollection[i].className.split(" ");
-  console.log(currentItem);
-  // console.log(classCollection[i].className);
-  if (currentItem.includes('vector')) {
-    console.log('нашел вектор')
-  }
-  // checkClass(current[i]);
-}
-// function checkClass (class) {
-//   if class =
-// }
-//
-function addClass (item) {
 
-}
+function checkFilter (checkedClasses) {
+  for (var s = 0; s < itemCollection.length; s++) {
+    var currentItem = itemCollection[s];                          // запоминаю какое фото проверяем
+    currentItemClasses = itemCollection[s].className.split(" ");  // создаю массив классов текущего фото
 
-
-
-// Add active class to the current button (highlight it)
-var btns = document.getElementsByClassName("portfolio-filter__checkbox");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.querySelectorAll("input[type=checkbox]:checked");
-    for (var i = 0; i < current.length; i++) {
-      console.log(current[i].name);
-      // checkClass(current[i]);
+    if (currentItemClasses.includes('show')) {                    //  очищаю поле коллекции от выбранных прошлый раз элементов
+      removeClass (currentItem);
     }
-    // current[0].className = current[0].className.replace(" active", "");
-    // this.className += " active";
+
+    for (var p = 0; p < checkedClasses.length; p++) {             //  перебираю классы выбранных вкладок
+      if (currentItemClasses.includes(checkedClasses[p].name)) {  //  если массив фото содержит класс вкладки
+        addClass (currentItem);                                   // перекидываю фото в добавление класса
+      }
+    }
+
+  }
+}
+
+
+function addClass (item) {
+  item.classList.add('show');
+}
+
+function removeClass (item) {
+  item.classList.remove('show');
+}
+
+var btnsFilter = document.getElementsByClassName("portfolio-filter__checkbox");
+for (var i = 0; i < btnsFilter.length; i++) {
+  btnsFilter[i].addEventListener("click", function()  {           // навешиваю обработчики на кнопки фильтра
+    var currentChecked = document.querySelectorAll("input[type=checkbox]:checked");       // при клике отбираю включенные (checked) вкладки
+    checkFilter (currentChecked);                                 // отправляю в функцию сравнения коллекции с включенными вкладками фильтра
   });
 }

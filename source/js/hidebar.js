1aa39cf4be7ec179e098ec855b8eef9f772082fd
sphini <spdop2000@gmail.com>
2020-05-12 20:51:59 +0500

    'чек'

--- source/js/hidebar.js
+++ source/js/hidebar.js
@@ -1,13 +1,39 @@
 /* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
 var pageNav = document.querySelector(".page-header__nav");
 
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    pageNav.style.top = "0";
-  } else {
-    pageNav.style.top = "-150px";
+
+
+
+if (document.documentElement.clientWidth >= 768) {
+
+  var prevScrollpos = window.pageYOffset;
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+      pageNav.style.top = "0";
+    } else {
+      pageNav.style.top = "-250px";
+    }
+    prevScrollpos = currentScrollPos;
   }
-  prevScrollpos = currentScrollPos;
+  console.log(document.documentElement.clientWidth);
+
+
+} else {
+
+
+var headerPage = document.querySelector('.page-header');
+var headerToggle = document.querySelector('.page-header__toggle');
+
+
+headerToggle.addEventListener('click', function() {
+  if (headerPage.classList.contains('page-header--mobile-closed')) {
+    headerPage.classList.remove('page-header--mobile-closed');
+    headerPage.classList.add('page-header--mobile-opened');
+  } else {
+    headerPage.classList.add('page-header--mobile-closed');
+    headerPage.classList.remove('page-header--mobile-opened');
+  }
+});
+
 }
